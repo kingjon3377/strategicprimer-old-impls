a330a8508a904083f730ad0cf84c46b4cddabf03
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2009-06-10 11:00:36 -0400

    Remove duplicate declarations of abstract methods.

--- sp_java_orig/sp/model/Module.java
+++ sp_java_orig/sp/model/Module.java
@@ -109,41 +109,6 @@ public abstract class Module implements IModule, Weapon, Statistician {
 	}
 
 	/**
-	 * Attack a specified module. At present caller is responsible
-	 * for checking whether this attack is valid.
-	 * TODO: Fix this with a server-side check.
-	 * Subclasses are responsible for ensuring this actually does
-	 * anything.
-	 *
-	 * @param target The target module.
-	 */
-	public abstract void attack(Module target);
-
-	/**
-	 * Checks whether it is possible to attack the specified
-	 * module. Subclasses are responsible for implementing this
-	 * stub; here this returns false. TODO: This should check that
-	 * the module can attack the target, and that the target can
-	 * be attacked by the module. 
-	 *
-	 * @param target The potential target
-	 * @return whether the attack is possible.
-	 */
-	public abstract boolean checkAttack(Module target);
-
-	/**
-	 * Check whether it is possible to make a ranged attack
-	 * against the specified module. TODO: Roll this into
-	 * checkAttack(). Subclasses are responsible for implementing
-	 * this; here it just returns false.
-	 *
-	 * @param target The potential target
-	 * @return whether a ranged attack against the target is
-	 * possible.
-	 */
-	public abstract boolean checkRangedAttack(Module target);
-
-	/**
 	 * @return the module's defense stat
 	 */
 	public int getDefense() {
@@ -236,19 +201,6 @@ public abstract class Module implements IModule, Weapon, Statistician {
 	 * @return whether the module can move
 	 */
 	public abstract boolean mobile();
-
-	/**
-	 * Make a ranged attack on a specified module. This is
-	 * temporary until we get a properly modular version of
-	 * attack() that checks distance and which would be better.
-	 * Subclasses are responsible for replacing this stub; caller
-	 * is at present responsible for checking whether this is
-	 * valid.  TODO: fold this into attack() with a proper
-	 * distance- and "stat"-checking algorithm.
-	 * 
-	 * @param target The target module
-	 */
-	public abstract void rangedAttack(Module target);
 
 	/**
 	 * @param _defense
