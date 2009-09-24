bfdcd69889826376e589d2b5ab26485cd2dee693
Jonathan Lovelace <kingjon3377@gmail.com>
2009-09-24 13:29:10 -0400

    Began actually implementing the GUI map.

--- sp_java_next/view/map/GUITile.java
+++ sp_java_next/view/map/GUITile.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 
+import model.location.Tile;
+
 /**
  * The graphical representation of a tile.
  * 
@@ -15,11 +17,29 @@ public class GUITile extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -189052820681357406L;
-
 	/**
 	 * Constructor, to make warnings go away.
+	 * @param _tile The tile this is the GUI for
 	 */
-	public GUITile() {
+	public GUITile(final Tile _tile) {
 		super(new BorderLayout());
+		tile = _tile;
+	}
+	/**
+	 * The tile this is the GUI representation of
+	 */
+	private Tile tile;
+	/**
+	 * @return The tile this is the GUI representation of
+	 */
+	public Tile getTile() {
+		return tile;
+	}
+	/**
+	 * @param _tile The new tile this should represent
+	 */
+	public void setTile(final Tile _tile) {
+		tile = _tile;
+		repaint();
 	}
 }
