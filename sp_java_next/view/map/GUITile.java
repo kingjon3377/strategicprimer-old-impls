ef39affa5653b4426a4188a37bd8e79c3031ffb0
Jonathan Lovelace <kingjon3377@gmail.com>
2009-09-24 15:41:01 -0400

    Added the TileType class from the Viewer project.

--- sp_java_next/view/map/GUITile.java
+++ sp_java_next/view/map/GUITile.java
@@ -1,6 +1,9 @@
 package view.map;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.ImageProducer;
+import java.io.IOException;
 
 import javax.swing.JPanel;
 
@@ -17,29 +20,49 @@ public class GUITile extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -189052820681357406L;
+
 	/**
 	 * Constructor, to make warnings go away.
-	 * @param _tile The tile this is the GUI for
+	 * 
+	 * @param _tile
+	 *            The tile this is the GUI for
 	 */
 	public GUITile(final Tile _tile) {
 		super(new BorderLayout());
-		tile = _tile;
+		setTile(_tile);
 	}
+
 	/**
 	 * The tile this is the GUI representation of
 	 */
 	private Tile tile;
+
 	/**
 	 * @return The tile this is the GUI representation of
 	 */
 	public Tile getTile() {
 		return tile;
 	}
+
 	/**
-	 * @param _tile The new tile this should represent
+	 * @param _tile
+	 *            The new tile this should represent
 	 */
-	public void setTile(final Tile _tile) {
+	public final void setTile(final Tile _tile) {
 		tile = _tile;
+		try {
+			terrainImage = createImage((ImageProducer) getClass().getResource(
+					getTerrainTypeString(_tile)).getContent());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		repaint();
+	}
+
+	private transient Image terrainImage;
+
+	private static String getTerrainTypeString(Tile _tile) {
+		throw new IllegalStateException("Unimplemented");
 	}
 }
