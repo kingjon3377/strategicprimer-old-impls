33eb3b1c4841d0a8f6b085128505290f4ac73960
Jonathan Lovelace <kingjon3377@gmail.com>
2009-11-12 13:32:06 -0500

    Second commit of the multiple-modules-per-location series: NullLocation.

--- sp_java_next/model/location/NullLocation.java
+++ sp_java_next/model/location/NullLocation.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import model.module.Module;
+import model.module.kinds.RootModule;
 
 /**
  * The null location.
@@ -20,7 +21,7 @@ public final class NullLocation implements Location {
 	public static final NullLocation NULL_LOC = new NullLocation();
 
 	/**
-	 * Version UID for serialization. 
+	 * Version UID for serialization.
 	 */
 	private static final long serialVersionUID = 4346702324428423006L;
 
@@ -37,9 +38,11 @@ public final class NullLocation implements Location {
 	}
 
 	/**
-	 * Do nothing -- this is the null location.
-	 * FIXME: Should modules be able to be "nowhere" and thus here?
-	 * @param module a module, ignored
+	 * Do nothing -- this is the null location. FIXME: Should modules be able to
+	 * be "nowhere" and thus here?
+	 * 
+	 * @param module
+	 *            a module, ignored
 	 */
 	public void add(final Module module) {
 		members.add(module);
@@ -49,7 +52,8 @@ public final class NullLocation implements Location {
 	 * The null location contains no other locations. So
 	 * 
 	 * @return false
-	 * @param location a location, ignored
+	 * @param location
+	 *            a location, ignored
 	 */
 	public boolean contains(final Location location) {
 		return false;
@@ -66,32 +70,62 @@ public final class NullLocation implements Location {
 
 	/**
 	 * Do nothing -- this is the null location.
-	 * @param module a module, ignored
+	 * 
+	 * @param module
+	 *            a module, ignored
 	 */
 	public void remove(final Module module) {
 		members.remove(module);
 	}
-	
+
 	/**
 	 * @return a <i>read-only copy</i> of the modules here
 	 */
 	public Set<Module> getMembers() {
 		return Collections.unmodifiableSet(members);
 	}
-	
+
 	/**
 	 * (Name misleading to mislead static analysis plugins.).
-	 * @param newMembers A set to join to the set of modules here.
+	 * 
+	 * @param newMembers
+	 *            A set to join to the set of modules here.
 	 */
 	public void setMembers(final Set<Module> newMembers) {
 		members.addAll(newMembers);
 	}
+
 	/**
-	 * @param module a module to pretend to add
+	 * @param module
+	 *            a module to pretend to add
 	 * @return that it's possible to add it to this location.
 	 */
 	@Override
 	public boolean checkAdd(final Module module) {
 		return true;
+	}
+
+	/**
+	 * @return the modules in the null location.
+	 */
+	@Override
+	public Set<Module> getModules() {
+		return getMembers();
+	}
+
+	/**
+	 * @param module
+	 *            ignored
+	 */
+	@Override
+	public void select(final Module module) {
+		throw new IllegalStateException("The null location can't have a module selected");
+	}
+	/**
+	 * @return the Module equivalent of null
+	 */
+	@Override
+	public Module getSelected() {
+		return RootModule.ROOT_MODULE;
 	}
 }
