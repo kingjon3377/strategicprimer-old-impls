b5ff11288421407692eaadd919e8e34e8af71398
kingjon <kingjon@myrriddium>
2010-04-13 21:35:58 -0400

    Have the view draw the map. This draws all tiles the same at present, but the map is the right size.

--- sp-ng/view/gui/MapPanel.java
+++ sp-ng/view/gui/MapPanel.java
@@ -1,5 +1,8 @@
 package view.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 import model.map.SPMap;
@@ -11,9 +14,21 @@ import view.util.Window;
  */
 public class MapPanel extends JPanel {
 	/**
+	 * Version UID for serialization.
+	 */
+	private static final long serialVersionUID = 7688251350376774189L;
+	/**
 	 * The map this represents
 	 */
 	private SPMap theMap;
+	/**
+	 * How wide a tile will be on this view. TODO: make this changeable at runtime.
+	 */
+	private static final int TILE_WIDTH = 10;
+	/**
+	 * How tall a tile will be on this view. TODO: make this changeable at runtime.
+	 */
+	private static final int TILE_HEIGHT = 10;
 	/**
 	 * Constructor.
 	 * @param map the map
@@ -33,5 +48,18 @@ public class MapPanel extends JPanel {
 	 */
 	public static void main(final String[] args) {
 		new Window(new MapPanel(new SPMap())).setVisible(true);
+	}
+	@Override
+	public void paint(final Graphics pen) {
+		Color origColor = pen.getColor();
+		for (int row = 0; row < theMap.getRows(); row++) {
+			for (int col = 0; col < theMap.getCols(); col++) {
+				pen.setColor(Color.blue);
+				pen.fillRect(col * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+				pen.setColor(Color.black);
+				pen.drawRect(col * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT);
+			}
+		}
+		pen.setColor(origColor);
 	}
 }
