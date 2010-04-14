53e8f4f08a8bd0c11bec7c208cae7988c4b1ebba
kingjon <kingjon@myrriddium>
2010-04-13 21:12:32 -0400

    Added a helper class to make frames out of panels.

---
+++ sp-ng/view/util/Window.java
@@ -0,0 +1,29 @@
+package view.util;
+
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+/**
+ * A utility class to make a window out of a panel or other component.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public class Window extends JFrame {
+	/**
+	 * Version UID for serialization.
+	 */
+	private static final long serialVersionUID = -8702989963712249998L;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param comp
+	 *            the component to wrap
+	 */
+	public Window(final Component comp) {
+		super();
+		add(comp);
+	}
+}
