3f9d11fe4492a502db29f61e3b3665eea26e1297
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2010-04-15 12:48:20 -0400

    A first implementation of the text UI.

--- sp-ng/view/tui/TextMap.java
+++ sp-ng/view/tui/TextMap.java
@@ -20,11 +20,27 @@ public class TextMap {
 	/**
 	 * How many characters wide each tile's representation will be.
 	 */
-	private static final int TILE_WIDTH = 4;
+	private final int tileWidth;
 	/**
 	 * How many rows of characters tall each tile's representation will be.
 	 */
-	private static final int TILE_HEIGHT = 4;
+	private final int tileHeight;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param map
+	 *            the map
+	 * @param tWidth
+	 *            how many characters wide to draw each tile
+	 * @param tHeight
+	 *            how many characters high to draw each tile
+	 */
+	public TextMap(final SPMap map, final int tWidth, final int tHeight) {
+		theMap = map;
+		tileWidth = tWidth;
+		tileHeight = tHeight;
+	}
 
 	/**
 	 * Constructor.
@@ -33,7 +49,7 @@ public class TextMap {
 	 *            the map
 	 */
 	public TextMap(final SPMap map) {
-		theMap = map;
+		this(map, 2, 2);
 	}
 
 	/**
@@ -58,6 +74,52 @@ public class TextMap {
 	 * 
 	 * @param out
 	 */
-	private void paint(PrintStream out) {
+	public void paint(PrintStream out) {
+		if (tileHeight < 2 && tileWidth < 2) {
+			for (int row = 0; row < theMap.getRows(); row++) {
+				for (int col = 0; col < theMap.getCols(); col++) {
+					paintTile(theMap.terrainAt(row, col), out);
+				}
+				out.append('\n');
+			}
+		} else if (tileHeight < 2 || tileWidth < 2) {
+			throw new IllegalStateException(
+					"Text tile height and width must either be both 1 or both greater than 1");
+		} else {
+			for (int row = 0; row < theMap.getRows(); row++) {
+				for (int j = 0; j < tileWidth * theMap.getCols(); j++) {
+					out.append('-');
+				}
+				out.append('\n');
+				for (int i = 1; i < tileHeight; i++) {
+					for (int col = 0; col < theMap.getCols(); col++) {
+						out.append('|');
+						for (int j = 1; j < tileWidth; j++) {
+							paintTile(theMap.terrainAt(row, col), out);
+						}
+					}
+					out.append('|');
+					out.append('\n');
+				}
+			}
+			for (int j = 0; j < tileWidth * theMap.getCols(); j++) {
+				out.append('-');
+			}
+			out.append('\n');
+		}
+	}
+
+	/**
+	 * Display a tile. Actually, since we can't go back to previous lines in
+	 * unbuffered stream I/O, most tile drawing is handled in the main
+	 * map-drawing method.
+	 * 
+	 * @param tile
+	 *            The tile to draw.
+	 * @param out
+	 *            The stream to draw to.
+	 */
+	private void paintTile(int tile, PrintStream out) {
+		out.append('.');
 	}
 }
