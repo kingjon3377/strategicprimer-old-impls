eafa66dc1da2d9a74311ef12fbb0579c39e375ba
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2010-04-17 11:49:35 -0400

    A class to read a map from file. Not yet implemented.

---
+++ sp-ng/controller/util/MapReader.java
@@ -0,0 +1,19 @@
+package controller.util;
+
+import model.map.SPMap;
+
+/**
+ * A class to read a map from file.
+ * @author Jonathan Lovelace
+ *
+ */
+public class MapReader {
+	/**
+	 * Read a map from a file.
+	 * @param filename the file to read from
+	 * @return the map in that file
+	 */
+	public SPMap readMap(final String filename) {
+		return new SPMap();
+	}
+}
