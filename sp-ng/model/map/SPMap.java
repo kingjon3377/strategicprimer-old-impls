ac453dd08a8b66f851073932484c2e50ad6ccc86
kingjon <kingjon@myrriddium>
2010-04-20 19:56:41 -0400

    Added deep equality of SPMaps. That should probably be tested too.

--- sp-ng/model/map/SPMap.java
+++ sp-ng/model/map/SPMap.java
@@ -1,5 +1,7 @@
 package model.map;
 
+import java.util.Arrays;
+
 /**
  * The model class representing the map. At first this will just be the terrain
  * at each position, but other data will be added later.
@@ -53,5 +55,25 @@ public class SPMap {
 	 */
 	public final int getCols() {
 		return theMap[0].length;
+	}
+
+	/**
+	 * @param map
+	 *            another map
+	 * @return whether it is the same as this one
+	 */
+	@Override
+	public boolean equals(final Object map) {
+		return map instanceof SPMap && getRows() == ((SPMap) map).getRows()
+				&& getCols() == ((SPMap) map).getCols()  
+				&& Arrays.deepEquals(theMap, ((SPMap) map).theMap);
+	}
+
+	/**
+	 * @return a hashcode, since we override equals
+	 */
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(theMap);
 	}
 }
