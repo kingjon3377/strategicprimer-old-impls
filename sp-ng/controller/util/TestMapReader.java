ef4bd6cba23f0fa488903d380960eb60893726ae
kingjon <kingjon@myrriddium>
2010-04-20 19:57:32 -0400

    First tests of the map-reading code; I\'ll mark the feature done. There should really be some more testing, and error-handling code leaves a great deal to be desired.

--- sp-ng/controller/util/TestMapReader.java
+++ sp-ng/controller/util/TestMapReader.java
@@ -1,8 +1,12 @@
 package controller.util;
 
+import static org.junit.Assert.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+
+import model.map.SPMap;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,5 +24,19 @@ public class TestMapReader {
 	@Test(expected=IllegalArgumentException.class)
 	public void testRejectsZeroWidthMap() throws IOException {
 		reader.readMap(new BufferedReader(new StringReader("2 0")));
+	}
+	@Test(expected=Exception.class)
+	public void testRejectsZeroLengthInput() throws IOException {
+		reader.readMap(new BufferedReader(new StringReader("")));
+	}
+	@Test(expected=Exception.class)
+	public void testRejectsTooShortMap() throws IOException {
+		reader.readMap(new BufferedReader(new StringReader("1 1")));
+	}
+	@Test
+	public void testReturnsProperResult() throws IOException {
+		SPMap map = new SPMap(new int[][] {{1,2},{3,4}});
+		assertTrue(map.equals(reader.readMap(new BufferedReader(new StringReader("2 2 1 2 3 4")))));
+		assertEquals(map,reader.readMap(new BufferedReader(new StringReader("2 2 1 2 3 4"))));
 	}
 }
