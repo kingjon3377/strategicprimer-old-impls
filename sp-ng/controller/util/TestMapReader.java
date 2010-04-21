3e39bce8a9edbfba1606b85b6eb8b874b2562c02
kingjon <kingjon@myrriddium>
2010-04-20 20:48:11 -0400

    Fixed warnings, mostly by adding documentation, in the TestMapReader test-case class.

--- sp-ng/controller/util/TestMapReader.java
+++ sp-ng/controller/util/TestMapReader.java
@@ -10,29 +10,64 @@ import model.map.SPMap;
 
 import org.junit.Before;
 import org.junit.Test;
-
-public class TestMapReader {
+/**
+ * A class to test the MapReader class.
+ * @author Jonathan Lovelace
+ */
+public final class TestMapReader {
+	/**
+	 * Constructor, to make a warning go away.
+	 */
+	public TestMapReader() {
+		setUp();
+	}
+	/**
+	 * The object we're testing.
+	 */
 	private MapReader reader;
+	/**
+	 * Set up for the tests.
+	 */
 	@Before
 	public void setUp() {
 		reader = new MapReader();
 	}
+	/**
+	 * Tests that a map of zero height will be rejected. 
+	 * @throws IOException "Thrown" by the method we're testing
+	 */
 	@Test(expected=IllegalArgumentException.class)
 	public void testRejectsZeroHeightMap() throws IOException {
 		reader.readMap(new BufferedReader(new StringReader("0 1")));
 	}
+	/**
+	 * Tests that a map of zero width will be rejected.
+	 * @throws IOException "Thrown" by the method we're testing
+	 */
 	@Test(expected=IllegalArgumentException.class)
 	public void testRejectsZeroWidthMap() throws IOException {
 		reader.readMap(new BufferedReader(new StringReader("2 0")));
 	}
+	/**
+	 * Tests that an empty string input will be rejected.
+	 * @throws IOException "Thrown" by the method we're testing
+	 */
 	@Test(expected=Exception.class)
 	public void testRejectsZeroLengthInput() throws IOException {
 		reader.readMap(new BufferedReader(new StringReader("")));
 	}
+	/**
+	 * Tests that a too-short map will be rejected.
+	 * @throws IOException "Thrown" by the method we're testing
+	 */
 	@Test(expected=Exception.class)
 	public void testRejectsTooShortMap() throws IOException {
 		reader.readMap(new BufferedReader(new StringReader("1 1")));
 	}
+	/**
+	 * Tests that the reader actually produces the proper result.
+	 * @throws IOException "Thrown" by the method we're testing
+	 */
 	@Test
 	public void testReturnsProperResult() throws IOException {
 		SPMap map = new SPMap(new int[][] {{1,2},{3,4}});
