b2dd64e35a1dc52d8c2aa053a1ddadff46eb67a6
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2010-04-20 22:36:28 -0400

    Enumerated possible tile types.

---
+++ sp-ng/model/map/TileType.java
@@ -0,0 +1,35 @@
+package model.map;
+
+/**
+ * Tile types.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public enum TileType {
+	/**
+	 * Ocean or river. This will eventually be determined by elevation and water
+	 * table.
+	 */
+	WATER,
+	/**
+	 * Desert. TODO: How to distinguish between sandy and rocky desert?
+	 */
+	DESERT,
+	/**
+	 * Swamp. TODO: Should this be somehow, like water, be just a result of a
+	 * certain combination of elevation and water table?
+	 */
+	SWAMP,
+	/**
+	 * Plains. Underbrush when near trees.
+	 */
+	PLAINS,
+	/**
+	 * Ice or quasi-permanent snow.
+	 */
+	ICE;
+	/**
+	 * TODO: What about inside buildings?
+	 */
+}
