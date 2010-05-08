f46e187549e8f8a5754861f23a2eba624c5d6ed9
kingjon <kingjon@myrriddium>
2010-05-07 22:00:06 -0400

    Add an enumerated type for terrain objects, to replace mere ints.

---
+++ sp-ng/model/map/TerrainObject.java
@@ -0,0 +1,13 @@
+package model.map;
+
+/**
+ * An enumerated type for objects that can be on tiles.
+ * 
+ * @author Jonathan Lovelace
+ */
+public enum TerrainObject {
+	/**
+	 * A tree.
+	 */
+	TREE;
+}
