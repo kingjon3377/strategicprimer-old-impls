afc2da0e966d76362075dba73af7bdb2d947c747
Jonathan Lovelace <1522278+kingjon3377@users.noreply.github.com>
2010-05-15 17:59:18 -0400

    Added the ability to load a map at runtime.

--- sp-ng/view/gui/MapPanel.java
+++ sp-ng/view/gui/MapPanel.java
@@ -262,4 +262,12 @@ public class MapPanel extends JPanel implements PropertyChangeListener {
 			repaint();
 		}
 	}
+	/**
+	 * Load in a new map
+	 * @param map the new map
+	 */
+	public void setMap(final SPMap map) {
+		theMap = map;
+		repaint();
+	}
 }
