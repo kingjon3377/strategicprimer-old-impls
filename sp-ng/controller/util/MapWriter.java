425115341167c6a448a7dbcded6e8b8a1d1497a0
kingjon <kingjon@myrriddium>
2010-05-15 17:57:56 -0400

    Remove two unused imports.

--- sp-ng/controller/util/MapWriter.java
+++ sp-ng/controller/util/MapWriter.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import model.map.SPMap;
 import model.map.TerrainObject;
